34b7722a6541e59a1f53803cb83f4946dda6df05
Pando Deploy <deploy@pando.network>
2026-02-25 19:21:03 +0000

    Deploy 2026-02-25T19:21:03

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => res.send("I was misclassified as Tier 1!"));
+app.listen(port, () => console.log("Running on port " + port));
